8d9bd93866c26d4a0d8dfed41139644050a7bdc4
Alexey Kornilov <kavolorn@gmail.com>
2014-10-06 12:15:02 +0300

    Get value functionality has been implemented.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char ** argv)
     const char * config_string = "--SERVER=127.0.0.1";
     memcached_st * memc = memcached(config_string, strlen(config_string));
 
+    // Storing key value in memcached system
+    
     const char *key = "foo";
     const char *value = "value";
 
@@ -15,11 +17,30 @@ int main(int argc, char ** argv)
 
     if (rc != MEMCACHED_SUCCESS)
     {
-        cerr << "Memcached storing error.";
+        cerr << "Memcached storing error.\n";
+        return 1;
     }
     else
     {
-        cout << "Value has been written correctly.";
+        cout << "Value has been written correctly.\n";
+    }
+    
+    // Getting back stored value
+    
+    size_t value_length;
+    uint32_t flags;
+    memcached_return_t error;
+    
+    char * return_value = memcached_get(memc, key, strlen(key), &value_length, &flags, &error);
+    
+    if (error != MEMCACHED_SUCCESS)
+    {
+        cerr << "Memcached reading error.\n";
+        return 1;
+    }
+    else
+    {
+        cout << return_value << endl;
     }
     
     memcached_free(memc);
