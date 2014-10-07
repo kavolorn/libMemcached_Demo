4c28d795ab2f1d72a3d7dce289c14a04a187d539
Alexey Kornilov <kavolorn@gmail.com>
2014-10-07 11:50:04 +0300

    Example of working with increment has been added.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <ctime>
 #include "memcached.hpp"
 
 using namespace std;
 
 int main(int argc, char ** argv)
 {
-    const char * config_string = "--SERVER=127.0.0.1";
+    const char * config_string = "--SERVER=127.0.0.1 --BINARY-PROTOCOL";
     memcached_st * memc = memcached(config_string, strlen(config_string));
 
     // Storing key value in memcached system
     
-    const char *key = "foo";
-    const char *value = "value";
+    const char * key = "foo";
+    const char * value = "bar";
 
-    memcached_return_t rc = memcached_set(memc, key, strlen(key), value, strlen(value), (time_t)0, (uint32_t)0);
+    memcached_return_t rc = memcached_set(memc, key, strlen(key), value, strlen(value), 0, 0);
 
     if (rc != MEMCACHED_SUCCESS)
     {
@@ -29,11 +30,10 @@ int main(int argc, char ** argv)
     
     size_t value_length;
     uint32_t flags;
-    memcached_return_t error;
     
-    char * return_value = memcached_get(memc, key, strlen(key), &value_length, &flags, &error);
+    char * return_value = memcached_get(memc, key, strlen(key), &value_length, &flags, &rc);
     
-    if (error != MEMCACHED_SUCCESS)
+    if (rc != MEMCACHED_SUCCESS)
     {
         cerr << "Memcached reading error.\n";
         return 1;
@@ -42,6 +42,30 @@ int main(int argc, char ** argv)
     {
         cout << return_value << endl;
     }
+    
+    // Working with increment
+    
+    const char * inc_key = "var1";
+    uint64_t inc_variable = 0;
+    
+    time_t now = time( NULL);
+    struct tm now_tm = *localtime(&now);
+    struct tm then_tm = now_tm;
+    then_tm.tm_sec += 10;
+    
+    rc = memcached_increment_with_initial(memc, inc_key, strlen(inc_key), 1, 1, mktime(&then_tm), &inc_variable);
+    
+    if (rc != MEMCACHED_SUCCESS)
+    {
+        cerr << "Memcached increment error.\n" << memcached_strerror(memc, rc) << endl;
+        return 1;
+    }
+    else
+    {
+        cout << "Value has been incremented correctly.\n";
+    }
+    
+    cout << inc_variable << endl;
     
     memcached_free(memc);
     return 0;
